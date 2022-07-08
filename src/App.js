b6ca767bed20aa0271bf2841072870c8c3a4c5f5
ErikCvetkov <58137575+ErikCvetkov@users.noreply.github.com>
2022-07-08 16:40:00 +0300

    add order to a cart beta version

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ class App extends React.Component {
       currentCategory: "all",
     }
     this.chooseCategory = this.chooseCategory.bind(this)
+    this.addItemToOrder = this.addItemToOrder.bind(this)
   }
 
   render() {
     return (
       <div>
-        <Header chooseCategory={this.chooseCategory} />
-        <Items category={this.state.currentCategory} />
+        <Header chooseCategory={this.chooseCategory} orders={this.state.orders}/>
+        <Items category={this.state.currentCategory} addItemToOrder={this.addItemToOrder} />
       </div>
     )
   }
@@ -29,6 +30,21 @@ class App extends React.Component {
     })
   }
 
+  addItemToOrder(item) {
+    let isInArray = false
+    this.state.orders.forEach(el => {
+      if (el.id === item.id) {
+        isInArray = true
+      }
+    })
+    if (!isInArray) {
+      this.setState({ orders: [...this.state.orders, item] }, () => {
+        console.log(this.state.orders)
+      })
+    }
+  }
 }
+
+
 
 export default App;
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,5 +1,41 @@
 import React, { Component } from 'react'
 import Item from './Item'
+import { gql } from '@apollo/client';
+import { Query } from '@apollo/client/react/components';
+
+const POSTS_ITEMS = gql`
+query items {
+    category{
+      name
+      products{
+        id
+        name
+        gallery
+        category
+        prices{
+          currency{
+            symbol
+          }
+          amount
+        }
+        attributes{
+            id
+            name
+            type
+            items{
+              displayValue
+              id
+              value
+            }
+        }
+        inStock
+      }
+    }
+  }
+`;
+
+const filter = (data, filter) => (filter === "all") ? (data.category.products) : (data.category.products.filter((item) => item.category === filter))
+
 
 export class Items extends Component {
     render() {
@@ -9,7 +45,16 @@ export class Items extends Component {
                     {this.props.category.toUpperCase()}
                 </div>
                 <div className='row catalogue'>
-                    <Item category={this.props.category}/>
+                    <Query query={POSTS_ITEMS}>
+                        {({ loading, error, data }) => {
+                            if (loading) return <p>Loading...</p>;
+                            if (error) return <p>Error :(</p>;
+                            const items = filter(data, this.props.category)
+                            return items.map( el => (
+                                <Item key={el.id} item={el} addItemToOrder={this.props.addItemToOrder}/>
+                            ));
+                        }}
+                    </Query>
                 </div>
             </main>
         )
