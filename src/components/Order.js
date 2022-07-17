575c8730f43082b897dcdb635ff449c60609509f
ErikCvetkov <58137575+ErikCvetkov@users.noreply.github.com>
2022-07-17 18:22:15 +0300

    order image switcher added

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import Price from './Price'
 
 export class Order extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            currantImage: 0
+        }
+        this.switchImage = this.switchImage.bind(this)
+    }
     render() {
         const el = this.props.el
         const index = this.props.index
@@ -72,11 +79,38 @@ export class Order extends Component {
                         <button className='button-count' onClick={() => this.props.updateOrderCount(index, false)}>-</button>
                     </div>
                     <div className='item-img' style={{
-                        backgroundImage: `url(${el.gallery[0]})`
-                    }}></div>
+                        backgroundImage: `url(${el.gallery[this.state.currantImage]})`
+                    }}>
+                        {this.props.id === 'cartOverview' &&
+                            <div className='switcher'>
+                                <button className='switcher-button' onClick={()=>this.switchImage('left')}>
+                                    <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M7.25 1.06857L1.625 6.6876L7.25 12.3066" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </button>
+                                <button className='switcher-button' onClick={()=>this.switchImage('right')}>
+                                    <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M0.75 1.06808L6.375 6.68711L0.75 12.3062" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </button>
+                            </div>
+                        }
+                    </div>
                 </div>
             </div>
         )
+    }
+    switchImage(move){
+        let imageCount = this.props.el.gallery.length
+        if (move === 'left' && this.state.currantImage !== 0){
+            this.setState({currantImage: this.state.currantImage - 1})
+        } else if (move === 'left' && this.state.currantImage === 0){
+            this.setState({currantImage: imageCount-1})
+        } else if (move === 'right' && this.state.currantImage !== imageCount-1){
+            this.setState({currantImage: this.state.currantImage + 1})
+        } else if (move === 'right' && this.state.currantImage === imageCount-1) {
+            this.setState({currantImage: 0})
+        }
     }
 }
 
